c88495c324732c07b1b3f6dae4b642c51aa06f75
Barasa <timbaros87@gmail.com>
2024-06-18 12:38:29 +0300

    Add useOutletContext hook in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,14 @@
-import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import UserCard from "../components/UserCard";
 
-function Home() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
-  }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>;
-  });
+function Home(){
+    const users = useOutletContext();
+    const userList = users.map(user => <UserCard key={user.id} user={user}/>);
 
   return (
-         <main>
+      <main>
         <h1>Home!</h1>
-        <Outlet/>
+        <Outlet />
         {userList}
       </main>
   );
